9a2d6f3c54e68bfc86e72864d25054d01683b35e
Daniel Denton <danieldenton@Daniels-MBP-2.lan>
2023-07-26 13:13:25 -0700

    first constructor

--- hello.ts
+++ hello.ts
@@ -3,7 +3,22 @@ interface User {
     id: number;
 }
 
-const user: User = {
-    name: "Bruce",
-    id: 0
+class UserAccount {
+    name: string;
+    id: number;
+
+    constructor(name: string, id: number) {
+        this.name = name;
+        this.id = id;
+    }
 }
+
+const user : User = new UserAccount("River", 1)
+
+
+
+// const user: User = {
+//     name: "Bruce",
+//     id: 0
+// }
+
