0fc7ebe84e742caec3a25974c952341ab9e9e080
Daniel Denton <danieldenton@Daniels-MBP-2.lan>
2023-08-30 16:48:01 -0700

    myVal

--- src/myfunction.ts
+++ src/myfunction.ts
@@ -3,6 +3,26 @@ function addTwo(num: number) {
     return num + 2
 }
 
+function getUpper(val: string) {
+    return val.toUpperCase()
+}
+
+
+function signUpUser(name: string, email: string, isPaid: boolean) {}
+
+let loginUser = (name: string, email: string, isPaid: boolean = false) => {}
+
+function getValue(myVal: number): boolean {
+    if (myVal > 5) {
+        return true
+    }
+    return false
+}
+
+getUpper("hey bud")
 addTwo(5)
+signUpUser('river', 'river@river.com', false) 
+loginUser('rp', 'r@p.com')
+getValue(7)
 
 export {}
