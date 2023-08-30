b2f30b6a33344c8458d717521038541a8492b661
Daniel Denton <danieldenton@Daniels-MBP-2.lan>
2023-08-30 15:29:29 -0700

    number and boolean

---
+++ src/variableMe.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var greetings = "hey bud";
+console.log(greetings.indexOf("t"));
+// number not numbers, int or float
+var userId = 444.44;
+var isLoggedIn = false;
--- src/variableMe.ts
+++ src/variableMe.ts
@@ -1,1 +1,11 @@
 let greetings: string = "hey bud"
+
+console.log(greetings.indexOf("t"))
+
+// number not numbers, int or float
+let userId: number = 444.44
+
+// boolean
+let isLoggedIn: boolean = false
+
+export {}
