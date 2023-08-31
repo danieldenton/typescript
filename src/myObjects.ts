d56ba023dc274ebc8a91994608be947bcd263df1
Daniel Denton <danieldenton@Daniels-MBP-2.lan>
2023-08-30 17:22:30 -0700

    js weirdness

--- src/myObjects.ts
+++ src/myObjects.ts
@@ -7,7 +7,12 @@ const User =  {
 // the course is wrong. this is how an object is defined in TS.
 function createUser({ name, isPaid }: { name: string, isPaid: boolean }){} 
 
-createUser({name: "Joe", isPaid: true})
+// this below won't work
+// createUser({name: "Joe", isPaid: true, email: 'joe@joe.com'})
+
+// but this will: but it's not great.
+const newUser = {name: "Joe", isPaid: true, email: 'joe@joe.com'}
+createUser(newUser)
 
 function createCourse():{name: string, price: number}{
     return {name: "react", price: 666}
