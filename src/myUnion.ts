9c776f35e2a2995ddb483d5139b2f7c1361a82aa
Daniel Denton <danieldenton@Daniels-MBP-2.lan>
2023-08-31 11:33:05 -0700

    unions for arrays

--- src/myUnion.ts
+++ src/myUnion.ts
@@ -13,3 +13,19 @@ type Admin = {
 }
 
 let bruce: User | Admin = {name: 'Bruce', id: 444}
+
+// function getDbId(id: number | string){
+//     console.log(`DB id is ${id}`)
+// }
+// getDbId(4)
+// getDbId('4')
+
+function getDbId(id: number | string){
+    if (typeof id === "string") { 
+        id.toLowerCase()
+    }
+ }
+
+ const data: (number | string)[] = [1, 2, 3, "4"]
+    
+export {}
