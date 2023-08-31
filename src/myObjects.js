65d246b6bcd3e8738ac43c6f23cfbf86cb4cf772
Daniel Denton <danieldenton@Daniels-MBP-2.lan>
2023-08-30 17:16:31 -0700

    course corrected for outdated syntax

---
+++ src/myObjects.js
@@ -0,0 +1,8 @@
+var User = {
+    name: 'Bruce',
+    email: 'bruce@bruce.com',
+    isActive: true
+};
+function createUser(_a) {
+    var string = _a.name, boolean = _a.isPaid;
+}
--- src/myObjects.ts
+++ src/myObjects.ts
@@ -3,3 +3,12 @@ const User =  {
     email: 'bruce@bruce.com',
     isActive: true
 }
+
+// the course is wrong. this is how an object is defined in TS.
+function createUser({ name, isPaid }: { name: string, isPaid: boolean }){} 
+
+createUser({name: "Joe", isPaid: true})
+
+function createCourse():{}{}
+
+export {}
