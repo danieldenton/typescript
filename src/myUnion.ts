cb6a840cdafa2d875daf112af983bd0409e8bddc
Daniel Denton <danieldenton@Daniels-MBP-2.lan>
2023-08-31 11:24:28 -0700

    union 2

--- src/myUnion.ts
+++ src/myUnion.ts
@@ -1,3 +1,15 @@
 let score: number | string = 33
 
 score = 333
+
+type User = {
+    name: string
+    id: number
+}
+
+type Admin = {
+    userName: string
+    id: number
+}
+
+let bruce: User | Admin = {name: 'Bruce', id: 444}
