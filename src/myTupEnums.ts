bc35689737047091767aac60856132e2edd6e068
Daniel Denton <danieldenton@Daniels-MBP-2.lan>
2023-08-31 21:18:10 -0700

    tuples 1

---
+++ src/myTupEnums.ts
@@ -0,0 +1,13 @@
+// const user: string[] = ["dd", ]
+
+let tUser: [string, number, boolean]
+
+// in a tuple the types must be assigned in the order are declared
+tUser = ['hey', 12, true]
+
+// same amount of variables as well.
+let rgb: [number, number, number] = [255, 123, 112]
+
+type User = [number, string]
+
+const newUser: User = [112, 'example@example.com']
