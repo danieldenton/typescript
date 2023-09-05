f5501eefe85e7724b18705e948246b8cdf2d5729
Daniel Denton <danieldenton@Daniels-MBP-2.lan>
2023-09-04 17:23:23 -0700

    enums 2

--- src/emuns.ts
+++ src/emuns.ts
@@ -1,11 +1,12 @@
 enum SeatChoice {
-    AISLE,
-    MIDDLE,
-    WINDOW,
-    FOURTH
+    AISLE = "aisle",
+    MIDDLE = "middle",
+    WINDOW = "window",
+    FOURTH = "fourth"
 }
 
 const ddSeat = SeatChoice.AISLE
 
 // enum value default starts at 0 like an array index. If you were to start at whatever number, it will autoincrement.
 // If it's not a number it can be a string but then everything needs a value. There's no default.
+//  you can put const inn front of enum to generate less JS code.
